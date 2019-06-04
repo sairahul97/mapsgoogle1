ab1299863df3fd8ea38433c40f7405291fdeefe1
sairahul97 <sairahulaaa@gmail.com>
2019-06-04 15:29:16 +1200

    added fused location package and trying to check permissions

--- app/src/main/java/com/example/mapsgoogle1/MapsActivity.java
+++ app/src/main/java/com/example/mapsgoogle1/MapsActivity.java
@@ -1,8 +1,20 @@
 package com.example.mapsgoogle1;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,6 +24,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int REQUEST_LOCATION = 99;
     private GoogleMap mMap;
 
     @Override
@@ -22,6 +35,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        final FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+
+        LocationRequest req = new LocationRequest();
+        req.setInterval(2000); // 2 seconds
+        req.setFastestInterval(500); //500 milliseconds
+        req.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+
+
+        client.requestLocationUpdates(req, LocationCallback()){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                Log.e("location:", locationResult.getLastLocation().toString());
+
+                Toast.makeText(MapsActivity.this, locationResult.getLastLocation().toString(), Toast.LENGTH_LONG).show();
+// Add a marker in Sydney and move the camera
+                LatLng clocation = new LatLng(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude());
+                mMap.addMarker(new MarkerOptions().position(clocation).title("Current Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(clocation, 17.2f));
+
+            }
+
+        }
     }
 
 
